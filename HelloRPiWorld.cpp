a8fd74e1d3db8d7c50712a6c37524e7e471749bb
Krajcovic <dusan.krajcovic@gmail.com>
2015-05-14 14:04:20 +0200

    Remote debugging. Automatic debugging is ok, but must be used arm-linux-gnueabihf-gdb debuger

--- HelloRPiWorld.cpp
+++ HelloRPiWorld.cpp
@@ -7,11 +7,23 @@
 
 
 #include <iostream>
+#include <unistd.h>
 
 using namespace std;
 
 int main (void)
 {
+    int i = 0;
     cout << "Hello RPi Development World !"<< endl;
+
+    cout << "Hello RPI Development World Again !" << endl;
+
+    while(1)
+    {
+        cout << "I'm in the While loop and the value of variable i is: " << i << endl;
+        i++;
+        usleep(1000000); //wait for 1 seconds
+    }
+
     return 0;
 }
