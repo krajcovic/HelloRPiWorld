e7f0549a986d54de89a6e9c7904dc3baeab0b52f
Krajcovic <dusan.krajcovic@gmail.com>
2015-05-14 19:39:36 +0200

    Added libbcm2835 to project.

--- HelloRPiWorld.cpp
+++ HelloRPiWorld.cpp
@@ -5,25 +5,53 @@
  *      Author: krajcovic
  */
 
-
 #include <iostream>
 #include <unistd.h>
+#include <bcm2835.h>
 
 using namespace std;
 
-int main (void)
-{
-    int i = 0;
-    cout << "Hello RPi Development World !"<< endl;
+// Blinks on RPi Plug P1 pin 11 (which is GPIO pin 17)
+#define PIN RPI_GPIO_P1_11
+int main(int argc, char **argv) {
+	// If you call this, it will not actually access the GPIO
+	// Use for testing
+//    bcm2835_set_debug(1);
+	if (!bcm2835_init())
+		return 1;
+	// Set the pin to be an output
+	bcm2835_gpio_fsel(PIN, BCM2835_GPIO_FSEL_OUTP);
+	// Blink
+	while (1) {
+		// Turn it on
+		bcm2835_gpio_write(PIN, HIGH);
 
-    cout << "Hello RPI Development World Again !" << endl;
+		// wait a bit
+		bcm2835_delay(500);
 
-    while(1)
-    {
-        cout << "I'm in the While loop and the value of variable i is: " << i << endl;
-        i++;
-        usleep(1000000); //wait for 1 seconds
-    }
+		// turn it off
+		bcm2835_gpio_write(PIN, LOW);
 
-    return 0;
+		// wait a bit
+		bcm2835_delay(500);
+	}
+	bcm2835_close();
+	return 0;
 }
+
+//int main (void)
+//{
+//    int i = 0;
+//    cout << "Hello RPi Development World !"<< endl;
+//
+//    cout << "Hello RPI Development World Again !" << endl;
+//
+//    while(1)
+//    {
+//        cout << "I'm in the While loop and the value of variable i is: " << i << endl;
+//        i++;
+//        usleep(1000000); //wait for 1 seconds
+//    }
+//
+//    return 0;
+//}
