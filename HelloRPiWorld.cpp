a79176bdac35cb056b716ab6fe87dd2a51e2cd60
Krajcovic <dusan.krajcovic@gmail.com>
2015-05-14 13:16:39 +0200

    Example - test deploy eclipse app to rpi from remote pc.

---
+++ HelloRPiWorld.cpp
@@ -0,0 +1,17 @@
+/*
+ * HelloRPiWorld.cpp
+ *
+ *  Created on: May 14, 2015
+ *      Author: krajcovic
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main (void)
+{
+    cout << "Hello RPi Development World !"<< endl;
+    return 0;
+}
